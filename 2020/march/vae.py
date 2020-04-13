85d77f47dfe08917af4ca1de385907737083b13e
Peter Bloem <git@peterbloem.nl>
2020-04-13 21:31:17 +0200

    ...

--- 2020/march/vae.py
+++ 2020/march/vae.py
@@ -479,12 +479,62 @@ def go(arg):
                         ax.set_title('var')
                     plt.axis('off')
 
-
-
-
             plt.tight_layout()
-
             plt.savefig(f'reconstruction.{arg.rloss}.{epoch:03}.png')
+
+            if arg.zsize == 2: # latent space plot
+
+                N = 2000
+                # gather up first 200 batches into one big tensor
+                numbatches = N // arg.batch_size
+                images, labels = [], []
+                for i, (ims, lbs) in enumerate(testloader):
+                    images.append(ims)
+                    labels.append(lbs)
+
+                    if i > numbatches:
+                        break
+
+                images, labels = torch.cat(images, dim=0), torch.cat(labels, dim=0)
+
+                n, c, h, w = images.size()
+
+                z = encoder(images)
+                latents = z[:, :2].data
+
+                mn, mx = latents.min(), latents.max()
+                size = 1.0 * (mx - mn) / math.sqrt(n)
+                # Change 0.75 to any value between ~ 0.5 and 1.5 to make the digits smaller or bigger
+
+                fig = plt.figure(figsize=(8, 8))
+
+                # colormap for the images
+                norm = mpl.colors.Normalize(vmin=0, vmax=9)
+                cmap = mpl.cm.get_cmap('tab10')
+
+                for i in range(n):
+                    x, y = latents[i, 0:2]
+                    l = labels[i]
+
+                    im = images[i, :]
+                    alpha_im = im.permute(1, 2, 0).detach().cpu().numpy()
+                    color = cmap(norm(l))
+                    color_im = np.asarray(color)[None, None, :3]
+                    color_im = np.broadcast_to(color_im, (h, w, 3))
+                    # -- To make the digits transparent we make them solid color images and use the
+                    #    actual data as an alpha channel.
+                    #    color_im: 3-channel color image, with solid color corresponding to class
+                    #    alpha_im: 1-channel grayscale image corrsponding to input data
+
+                    im = np.concatenate([color_im, alpha_im], axis=2)
+                    plt.imshow(im, extent=(x, x + size, y, y + size))
+
+                    plt.xlim(mn, mx)
+                    plt.ylim(mn, mx)
+
+                plt.savefig(f'latent.{arg.rloss}.{epoch:03}.png')
+
+
 
 
 if __name__ == "__main__":
