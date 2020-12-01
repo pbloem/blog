c5af332bcd55d1b1770218fc522e9c6336d1a599
Peter Bloem <git@peterbloem.nl>
2020-12-01 16:41:19 +0100

    ...

--- slurmtest/slurm_test.py
+++ slurmtest/slurm_test.py
@@ -5,12 +5,16 @@ def go():
 
     task_index  = int( os.environ['SLURM_PROCID'] )
     n_tasks     = int( os.environ['SLURM_NPROCS'] )
-    port        = int( os.environ['SLURM_STEP_RESV_PORTS'].split('-')[0] )
+    # port        = int( os.environ['SLURM_STEP_RESV_PORTS'].split('-')[0] )
 
-    tf_hostlist = [ ("%s:%s" % (host,port)) for host in hostlist.expand_hostlist( os.environ['SLURM_NODELIST']) ]
+    tf_hostlist = hostlist.expand_hostlist( os.environ['SLURM_NODELIST'] )
 
-    print(socket.gethostname())
-    print(tf_hostlist)
+    print('hostname: ', socket.gethostname())
+
+    print('host list', tf_hostlist)
+    print('n tasks', n_tasks)
+    print('task idx ', task_index)
+
 
 if __name__ == "__main__":
     fire.Fire(go)
