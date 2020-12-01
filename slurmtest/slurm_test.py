f67261ca41ac2b633d889bd3ab04d91224568729
Peter Bloem <git@peterbloem.nl>
2020-12-01 17:09:44 +0100

    ...

--- slurmtest/slurm_test.py
+++ slurmtest/slurm_test.py
@@ -9,12 +9,12 @@ def go():
 
     tf_hostlist = hostlist.expand_hostlist( os.environ['SLURM_NODELIST'] )
 
-    print('hostname: ', socket.gethostname())
+    with open(f'log-{task_index}', 'w') as file:
+        print('hostname: ', socket.gethostname(), file=file)
 
-    print('host list', tf_hostlist)
-    print('n tasks', n_tasks)
-    print('task idx ', task_index)
-
+        print('host list', tf_hostlist, file=file)
+        print('n tasks', n_tasks, file=file)
+        print('task idx ', task_index, file=file)
 
 if __name__ == "__main__":
     fire.Fire(go)
