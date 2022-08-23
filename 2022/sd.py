c0805ddcb4b87d3c7728c5deff2e54f52c3c7bd7
Peter Bloem <git@peterbloem.nl>
2022-08-23 14:32:47 +0200

    ...

--- 2022/sd.py
+++ 2022/sd.py
@@ -1,14 +1,18 @@
 from diffusers import StableDiffusionPipeline
-import torch
+import torch, fire
 
 TOKEN = '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' # my token, please generate your own if you copy this: https://huggingface.co/settings/tokens
 
-# get your token at https://huggingface.co/settings/tokens
-pipe = StableDiffusionPipeline.from_pretrained("CompVis/stable-diffusion-v1-4", use_auth_token=TOKEN)
-pipe.to('cuda')
+def go(prompt):
+    pipe = StableDiffusionPipeline.from_pretrained("CompVis/stable-diffusion-v1-4", use_auth_token=TOKEN)
+    pipe.to('cuda')
 
-for i in range(5):
-    prompt = "Something which is upside-down."
+    for i in range(5):
+        prompt = "Something which is upside-down."
 
-    image = pipe(prompt)["sample"][0]
-    image.save(f'prompt{i}.png')
+        image = pipe(prompt)["sample"][0]
+        image.save(f'prompt{i}.png')
+
+
+if __name__ == '__main__':
+    fire.Fire(go)
